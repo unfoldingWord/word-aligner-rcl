94402f2d914a93c075f45d49681f90ba30b08b2e
blm <mcleanb@photo-nomad.net>
2023-01-20 07:39:07 -0500

    Gathering together pieces from translation-helps-rcl and wordAlignment tool.

---
+++ styleguide.config.js
@@ -0,0 +1,24 @@
+const { name, version, repository } = require('./package.json')
+const { styles, theme } = require('./styleguide.styles')
+
+module.exports = {
+  title: `${name} v${version}`,
+  ribbon: {
+    url: repository,
+    text: 'View on GitHub',
+  },
+  styles,
+  theme,
+  components: 'src/components/**/[A-Z]*.js',
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+  },
+}
