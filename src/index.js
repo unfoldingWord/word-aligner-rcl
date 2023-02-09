d54c501f416751d0096776bce7795de15056c6ff
blm <mcleanb@photo-nomad.net>
2023-02-09 15:39:32 -0500

    working on exports and dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import WordAligner from './components/WordAligner'
+import * as AlignmentHelpers from './utils/alignmentHelpers'
+export {AlignmentHelpers, WordAligner}
