2ffa77711df81c99c2c56a9e496a665f52e59541
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2024-06-04 09:52:55 -0400

    added exports

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export {
   Checker,
 }
 
+export * as TranslationUtils from './utils/translations'
+export * as twArticleHelpers from './helpers/translationHelps/twArticleHelpers'
+export * as tnArticleHelpers from './helpers/translationHelps/tnArticleHelpers'
